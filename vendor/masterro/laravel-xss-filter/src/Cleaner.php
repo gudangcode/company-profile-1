3874a176d36c7b5b4a455ecb10b60a775fa8ac04
gudangcode.com@gmail.com <gudangcode.com@gmail.com>
2020-12-03 16:38:53 +0700

    v.1.0

---
+++ vendor/masterro/laravel-xss-filter/src/Cleaner.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MasterRO\LaravelXSSFilter;
+
+class Cleaner
+{
+	/**
+	 * @var string
+	 */
+	protected $scriptsAndIframesPattern = '/(<script.*script>|<frame.*frame>|<iframe.*iframe>|<object.*object>|<embed.*embed>)/isU';
+
+	/**
+	 * @var string
+	 */
+	protected $inlineListenersPattern = '/on[A-z]+=\".*\"(?=.*>)/isU';
+
+	/**
+	 * Clean
+	 *
+	 * @param string $value
+	 *
+	 * @return string
+	 */
+	public function clean(string $value): string
+	{
+		$value = $this->escapeScriptsAndIframes($value);
+		$value = $this->removeInlineEventListeners($value);
+
+		return $value;
+	}
+
+	/**
+	 * Escape Scripts And Iframes
+	 *
+	 * @param string $value
+	 *
+	 * @return string
+	 */
+	protected function escapeScriptsAndIframes(string $value): string
+	{
+		preg_match_all($this->scriptsAndIframesPattern, $value, $matches);
+
+		foreach (array_get($matches, '0', []) as $script) {
+			$value = str_replace($script, e($script), $value);
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Remove Inline Event Listeners
+	 *
+	 * @param string $value
+	 *
+	 * @return string
+	 */
+	protected function removeInlineEventListeners(string $value): string
+	{
+		$string = preg_replace($this->inlineListenersPattern, '', $value);
+
+		return !is_string($string) ? '' : $string;
+	}
+}
